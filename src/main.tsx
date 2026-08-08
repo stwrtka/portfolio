4fff77b1f91b1bb91126dd43ffc39b48109df7f3
Khadija <k.stewxrt@gmail.com>
2026-08-08 16:04:10 -0400

    refactor: fix image paths

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import ProjectCard from './components/ProjectCard.tsx'
 import Blog from './components/BlogLayout.tsx'
+import Logo from './assets/logo.svg'
+import FinalStory from './assets/3ds-side-by-side.png'
+import NomRoulette from './assets/nom-roulette-logo.svg'
+import Footer from './assets/footer.svg'
 
 import './index.css'
 
@@ -10,15 +14,15 @@ createRoot(document.getElementById('root')!).render(
     <div className="flex flex-col h-screen color-stone-100 no-scrollbar">
 
       <div className="left-14.75 top-95 absolute inline-flex flex-col justify-start items-start grow">
-        <img src='/src/assets/logo.svg'/>
+        <img src={Logo} alt="Khadija Stewart"/>
         <div id="projects" className="grid grid-cols-3 gap-20 p-10">
-          <ProjectCard icon='/src/assets/3ds-side-by-side.png' title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
-          <ProjectCard icon='/src/assets/nom-roulette-logo.svg' title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
+          <ProjectCard icon={FinalStory} title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
+          <ProjectCard icon={NomRoulette} title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
         </div>
       <div id="blog" className='pb-'>
         <Blog />
       </div>
-      <img className="sticky top-0" src='/src/assets/footer.svg'/>
+      <img className="sticky top-0" src= {Footer}/>
     </div>
   </div>
   
