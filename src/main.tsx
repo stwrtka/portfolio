ead122ee90ad2572a2ff764f1b44f49b46b32c11
Khadija <k.stewxrt@gmail.com>
2026-08-08 17:37:16 -0400

    refactor: add footer and change background

--- src/main.tsx
+++ src/main.tsx
@@ -11,19 +11,22 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <div className="flex flex-col h-screen color-stone-100 no-scrollbar">
-
-      <div className="left-14.75 top-95 absolute inline-flex flex-col justify-start items-start grow">
-        <img src={Logo} alt="Khadija Stewart"/>
-        <div id="projects" className="grid grid-cols-3 gap-20 p-10">
-          <ProjectCard icon={FinalStory} title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
-          <ProjectCard icon={NomRoulette} title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
+    <div className="flex flex-col text-stone-100 no-scrollbar min-h-screen">
+      <div className='flex-1'>
+        <div className='inline-flex flex-col justify-start items-start grow pl-14.75 pt-100'>
+          <img src={Logo} alt="Khadija Stewart" className='scale-125 pl-10'/>
+          <div id="projects" className="grid grid-cols-3 gap-20 p-10">
+            <ProjectCard icon={FinalStory} title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
+            <ProjectCard icon={NomRoulette} title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
+          </div>
+        <div id="blog" className='pb-10'>
+          <Blog />
         </div>
-      <div id="blog" className='pb-'>
-        <Blog />
       </div>
-      <img className="sticky top-0" src= {Footer}/>
     </div>
+    <footer className='w-full'>
+      <img src={Footer} alt='Footer' className='w-full' />
+    </footer>
   </div>
   
   </StrictMode>
