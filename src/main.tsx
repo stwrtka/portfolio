5ed5fd29d2056800fdcea18115c3045aff947f75
Khadija <k.stewxrt@gmail.com>
2026-08-08 15:37:01 -0400

    refactor: fix image paths

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')!).render(
     <div className="flex flex-col h-screen color-stone-100 no-scrollbar">
 
       <div className="left-14.75 top-95 absolute inline-flex flex-col justify-start items-start grow">
-        <img src='src/assets/logo.svg'/>
+        <img src='/src/assets/logo.svg'/>
         <div id="projects" className="grid grid-cols-3 gap-20 p-10">
           <ProjectCard icon='/src/assets/3ds-side-by-side.png' title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
           <ProjectCard icon='/src/assets/nom-roulette-logo.svg' title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
@@ -18,7 +18,7 @@ createRoot(document.getElementById('root')!).render(
       <div id="blog" className='pb-'>
         <Blog />
       </div>
-      <img className="sticky top-0" src='/src\assets\footer.svg'/>
+      <img className="sticky top-0" src='/src/assets/footer.svg'/>
     </div>
   </div>
   
