af43ffc343dada0a0d6491de922253696d9981ea
Khadija <k.stewxrt@gmail.com>
2026-09-22 13:18:57 -0400

    fixed project links and colour of project text

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -6,7 +6,7 @@ type CardInfo = {
   title:string,
   tech_stack: string,
   description: string,
-  git_link: string,
+  link: string,
 }
 
 function ProjectCard(content: CardInfo) {
@@ -15,12 +15,12 @@ function ProjectCard(content: CardInfo) {
       return ( 
       <div className="w-80 h-30 relative overflow-hidden items-center">
         <div className="left-0 inline-flex justify-start">
-           <a href={content.git_link}>
-            <img className="scale-90"  src={content.icon} />
-            </a>
+          <img className="scale-90"  src={content.icon} />
           <div className="inline-flex flex-col justify-start gap-1">
-            <div className="self-stretch justify-starttext-base font-bold">{content.title}</div>
-            <div className="self-stretch justify-start text-sm font-normal">{content.tech_stack}</div>
+            <a href={content.link}>
+              <div className="self-stretch justify-start text-green text-base font-bold">{content.title}</div>
+            </a>
+            <div className="self-stretch justify-start text-sm text-pink font-normal">{content.tech_stack}</div>
             <div className="self-stretch justify-star text-xs font-normal">{content.description}</div>
           </div>
         </div>
@@ -30,11 +30,10 @@ function ProjectCard(content: CardInfo) {
   return ( 
     <div className="w-80 h-30 relative overflow-hidden items-center">
       <div className="left-0 inline-flex gap-3.5">
-        <a href={content.git_link}>
-          <img src={Bunny}/>
-        </a>
         <div className="inline-flex flex-col justify-start gap-1">
-          <div className="self-stretch justify-start text-stone-100 text-base font-bold">{content.title}</div>
+          <a href={content.link}>
+            <div className="self-stretch justify-start text-stone-100 text-base font-bold">{content.title}</div>
+          </a>
           <div className="self-stretch justify-start text-stone-100 text-base font-normal">{content.tech_stack}</div>
           <div className="self-stretch justify-start text-stone-100 text-xs font-normal">{content.description}</div>
         </div>
--- src/main.tsx
+++ src/main.tsx
@@ -12,26 +12,22 @@ createRoot(document.getElementById('root')!).render(
     <div className="flex flex-col text-white no-scrollbar min-h-screen font-body p-5">
       <div className='flex-1 pt-5'>
         <h1 className='text-5xl pb-5'>Khadija Stewart</h1>
-        <div className='border-y-2 w-120 py-2'>
-          <p>Computer Science, CTS & Math @ University of Guelph</p>
-        </div>
         <div className='border-b-2 w-120 py-2'>
-          <p>desc </p>
-          <p><br></br>Seeking 2027 Summer Internships</p>
+          <p>Computer Science, CTS & Math @ University of Guelph</p>
         </div>
         <div className='pt-2'>
           <ul>
-            <li>Activity 1 @</li>
-            <li>Activity 2 @</li>
-            <li>Activity 3 @</li>
+            <li>Curr. Graphic Design @ UofT Hacks</li>
+            <li>Prev. Undergraduate Research Assignment @ University of Guelph</li>
+            <p><br></br>Seeking 2027 Summer Internships</p>
           </ul>
         </div>
       </div>
       <p>Blog →</p>
     
       <div id="projects" className="flex grid-cols-3 gap-20">
-          <ProjectCard icon={FinalStory} title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' git_link='https://github.com/stwrtka/final-story'/>
-          <ProjectCard icon={NomRoulette} title='Nom Roulette' tech_stack='Figma, React' description="Can't decide what to eat?" git_link='https://github.com/stwrtka/Nom-Roulette'/>
+          <ProjectCard icon={FinalStory} title='Final Story' tech_stack='C, DevKitPro' description='3Ds Game' link='https://github.com/stwrtka/final-story'/>
+          <ProjectCard icon={NomRoulette} title='Nom Roulette' tech_stack='Figma' description="Can't decide what to eat?" link='https://www.figma.com/proto/gFWvx2QGcUpa9KjzOLQbNj/Nom-Roulette?node-id=106-410&p=f&viewport=151%2C392%2C0.12&t=CVQW6TUHVK1UMSC3-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=106%3A410&page-id=0%3A1'/>
       </div>
 
     <footer className='w-full p-2 static'>
