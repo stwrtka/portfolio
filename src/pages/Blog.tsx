25d4ec2613db5394f115ff2a3450ea4ca04eadc6
Khadija <k.stewxrt@gmail.com>
2026-09-25 12:39:58 -0400

    fix contacts on blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -10,8 +10,12 @@ function BlogView () {
           <a href='/' className='font-bold text-pink' >Home →</a>
         </div>
 
-    <footer className='w-full bg-accent pt-2 static'>
-      <p className='text-sm text-white'>©2026 Khadija Stewart. All Rights Reserved.</p>
+    <footer className='flex justify-between pt-2 static'>
+      <p className='text-xs text-white'>©2026 Khadija Stewart. All Rights Reserved.</p>
+      <div className='flex gap-5'>
+        <a href='https://www.linkedin.com/in/khadijastewart/'className='text-xs text-white underline'>Linkedin</a>
+        <a href='https://github.com/stwrtka'className='text-xs text-white underline'>GitHub</a>
+      </div>
     </footer>
   </body>
   )
